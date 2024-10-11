227163646caf3a05eba47ad4e47bbe212a622d5a
StellarStacker2 <csbs269058@saranathan.ac.in>
2024-10-11 08:01:11 +0530

    feat : added withdraw feature

--- Bankaccount.java
+++ Bankaccount.java
@@ -4,11 +4,20 @@ class Bankaccount{
 	public Bankaccount(int balance){
 		this.balance=balance;
 	}
-	private void deposit(int amount){
+	protected void deposit(int amount){
 		if(amount>0)	
 			balance+=amount;
 			System.out.println("Deposit successfull");
 		else
 			System.out.println("Amount cannot be negative");
-	}	
+	}
+	protected void withdraw(int amount){
+		if(amount<=balance){
+			balance-=amount;
+			System.out.println("Withdraw successfull");
+		}else
+			System.out.println("Insuuficent balance");
+		
+	}
+
 }
