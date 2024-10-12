22a8fbe4ccd40701c5b0c50b76fb0b92daa62d41
StellarStacker2 <csbs269058@saranathan.ac.in>
2024-10-12 12:01:50 +0530

    feat:Improved bounds of Frame

--- Myframe.java
+++ Myframe.java
@@ -3,7 +3,7 @@ import java.awt.event.WindowAdapter;
 class MyFrame extends Frame{
     public MyFrame(){
         super("MyBank");
-        setBounds(200,250,720,1080);
+        setBounds(-1,-1,1000,1000);
         setVisible(true);
     }
     public static void main(String[] args) {
