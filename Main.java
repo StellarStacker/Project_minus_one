a795677612d712db35c62a99c56391d2ae85f35d
StellarStacker <167179906+StellarStacker@users.noreply.github.com>
2024-10-17 20:53:41 +0530

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-class Main{
-     public static void main(String[] args) {
-        Create user1=new Create();
-        Bankaccount obj=user1.getobject();
-        obj.deposit(1000);
-        obj.checkbalance();
-    }   
-}
